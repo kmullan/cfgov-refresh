cfpb/consumerfinance.gov

Add an app to bring college-costs and comparison-tool under one roof, in Wagtail.

cfgov/college/disclosures/scripts/notifications.py
@@ -1,0 +1,62 @@
+import datetime
+from string import Template
+
+from django.core.mail import send_mail
+from django.utils import timezone
+
+from college.models import Notification
+
+
+INTRO = ('Notification failures \n'
+         'Notification delivery failed for the following offer IDs:\n\n')
+NOTE_TEMPLATE = Template(('Offer ID $oid:\n'
+                          '    timestamp: $time\n'
+                          '    app errors: $errors\n'
+                          'SEND LOG:\n$log\n'))
+
+
+def retry_notifications(days=1):
+    """attempt to resend recent notifications that failed"""
+
+    endmsg = ""
+    days_old = timezone.now() - datetime.timedelta(days=int(days))
+    failed_notifications = Notification.objects.filter(sent=False,
+                                                       timestamp__gt=days_old)
+    for each in failed_notifications:
+        endmsg += "{}\n".format(each.notify_school())
+    if not endmsg:
+        endmsg = "No failed notifications found"
+    return endmsg
+
+
+def send_stale_notifications(add_email=[]):
+    """Gather up notifications that have failed and are more than a day old."""
+
+    stale_date = timezone.now() - datetime.timedelta(days=1)
+    stale_notifications = Notification.objects.filter(sent=False,
+                                                      timestamp__lt=stale_date)
+    if not stale_notifications:
+        return "No stale notifications found"
+    contacts = {notification.institution.contact: [] for notification
+                in stale_notifications if notification.institution.contact}
+    for noti in stale_notifications:
+        payload = {
+            'oid': noti.oid,
+            'time': noti.timestamp.isoformat(),
+            'errors': noti.errors,
+            'log': noti.log
+        }
+        clist = contacts[noti.institution.contact]
+        clist.append(payload)
+    for contact in contacts:
+        msg = INTRO
+        for msgdict in contacts[contact]:
+            msg += NOTE_TEMPLATE.substitute(msgdict)
+        recipients = contact.contacts.split(',') + add_email
+        send_mail("CFPB notification failures",
+                  msg,
+                  "no-reply@cfpb.gov",
+                  recipients,
+                  fail_silently=False)
+        return ("Found {} stale notifications; emails sent to "
+                "{}".format(stale_notifications.count(), recipients))

cfgov/college/disclosures/scripts/purge_objects.py
@@ -1,0 +1,25 @@
+from college.models import Notification, Program
+
+
+error_msg = ("The only purge arguments that can be passed "
+             "are 'notifications', 'programs' and 'test-programs'")
+no_args_msg = ("You must supply an object type to purge: "
+               "notifications, programs or test-programs")
+object_map = {
+    'notifications': Notification.objects.all(),
+    'programs': Program.objects.all(),
+    'test-programs': Program.objects.filter(test=True),
+}
+
+
+def purge(objects):
+    """purge notification or program data"""
+    objects = objects.lower()
+    if not objects:
+        return no_args_msg
+    if objects not in object_map.keys():
+        return error_msg
+    queryset = object_map[objects]
+    msg = "Purging {} {}".format(queryset.count(), objects)
+    queryset.delete()
+    return msg

cfgov/college/management/commands/send_stale_notifications.py
@@ -1,0 +1,32 @@
+from django.core.management.base import BaseCommand
+
+from college.disclosures.scripts.notifications import (
+    send_stale_notifications
+)
+
+
+COMMAND_HELP = "Send_stale_notifications gathers up stale notifications -- "
+"those that are more than a day old and have failed to reach a school -- "
+"assembles details and a send log for each notification and emails that "
+"to the school and to D&D team members for troubleshooting."
+PARSER_HELP = "This command queries the database for notifications with "
+"a 'sent' value of False and a timestamp more than a day old. "
+"You can pass 'add-email' and a list of emails to send logs to in addition "
+"to the school contact."
+
+
+class Command(BaseCommand):
+    help = COMMAND_HELP
+
+    def add_arguments(self, parser):
+        parser.add_argument('--add-email',
+                            help=PARSER_HELP,
+                            nargs='+',
+                            type=str)
+
+    def handle(self, *args, **options):
+        if options['add_email']:
+            msg = send_stale_notifications(add_email=options['add_email'])
+        else:
+            msg = send_stale_notifications()
+        self.stdout.write(msg)

cfgov/college/management/commands/tag_schools.py
@@ -1,0 +1,23 @@
+from django.core.management.base import BaseCommand
+
+from college.disclosures.scripts import tag_settlement_schools
+
+
+COMMAND_HELP = """
+`tag_schools` updates the 'settlement_school' flag, which is used to mark
+ schools that are participating in the disclosure program pursuant
+ to a legal settlement. The command accepts one argument, an S3 URL, which
+ should point to a CSV of schools subject to settlement. The CSV must contain
+ columns for 'ipeds_unit_id' and 'flag'.
+"""
+
+
+class Command(BaseCommand):
+    help = COMMAND_HELP
+
+    def add_arguments(self, parser):
+        parser.add_argument('url', type=str)
+
+    def handle(self, *args, **options):
+        msg = tag_settlement_schools.tag_schools(options['url'])
+        self.stdout.write(msg)

cfgov/college/management/commands/update_pfc_national_stats.py
@@ -1,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from college.disclosures.scripts import nat_stats
+
+
+COMMAND_HELP = """update_pfc_national_stats gets the latest national statistics
+ yaml file from collegescorecard, parses it and updates our local json file at
+ college/fixtures/national_stats.json.
+"""
+
+
+class Command(BaseCommand):
+    help = COMMAND_HELP
+
+    def handle(self, *args, **options):
+        msg = nat_stats.update_national_stats_file()
+        self.stdout.write(msg)

cfgov/college/tests/test_validators.py
@@ -1,0 +1,106 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from college.validators import (
+    clean_boolean, clean_float, clean_integer, clean_string, clean_yes_no,
+    validate_uuid4
+)
+
+
+INVALID_UUID = """
+\r\n\r\nOMG CHECK OUT THIS TOTES LEGIT CFPB-APPROVED WEBSITE: /
+http://nyan.cat\r\n\r\nOh, and here is some Kafka:\r\n\r\nOne morning, /
+as Gregor Samsa was waking up from anxious dreams, /
+he discovered that in his bed he had been changed into a monstrous /
+verminous bug. He lay on his armour-hard back and saw, as he lifted /
+his head up a little, his brown, arched abdomen divided up into rigid /
+bow-like sections. From this height the blanket, just about ready /
+to slide off completely, could hardly stay in place. His numerous legs, /
+pitifully thin in comparison to the rest of his circumference, /
+flickered helplessly before his eyes.\r\n
+"""
+
+VALID_WORKSHEET_DATA = """{"1": \
+{"school":"University of Kansas",\
+"alias":"Jayhawks",\
+"netpriceok":500,\
+"control":"Public",\
+"oncampusavail":"Yes",\
+"tuitiongradoss":"",\
+"offerba":"Yes",\
+"state":"KS",\
+"books":500,\
+"online":"No",\
+"school_id":"155317"}}
+"""
+
+INVALID_WORKSHEET_DATA = """{"1": {"school": \
+"University of Kansas | Jayhawks'}}
+"""
+
+DIRTY_WORKSHEET_DATA = """{"1": {"school": \
+"<script>console.log('INVALID');</script>"}}
+"""
+
+
+class UUIDValidatorTestCase(TestCase):
+    def test_valid_uuid4(self):
+        result = validate_uuid4('841df17e-784f-4ea4-bfb3-ebac5d2fcfe5')
+        # UUID validator does not return a value, only indications are raising
+        # ValidationError, or not
+        self.assertEqual(result, None)
+
+    def test_invalid_uuid4(self):
+        self.assertRaises(ValidationError, validate_uuid4, INVALID_UUID)
+
+
+class WorkSheetValidatorTestCase(TestCase):
+
+    # def test_valid_worksheet_data(self):
+    #     result = validate_worksheet(VALID_WORKSHEET_DATA)
+    #     self.assertTrue('Jayhawks' in result)
+
+    # def test_invalid_worksheet_data(self):
+    #     self.assertRaises(ValidationError,
+    #                       validate_worksheet,
+    #                       INVALID_WORKSHEET_DATA)
+
+    # def test_dirty_worksheet_data(self):
+    #     result = validate_worksheet(DIRTY_WORKSHEET_DATA)
+    #     self.assertFalse('<' in result)
+
+    def test_clean_integer(self):
+        self.assertTrue(clean_integer(1) == 1)
+        self.assertTrue(clean_integer(1.1) == 1)
+        self.assertTrue(clean_integer(0) == 0)
+        self.assertTrue(clean_integer('c') == 0)
+        self.assertTrue(clean_integer(None) == 0)
+
+    def test_clean_float(self):
+        self.assertTrue(clean_float(0.1) == 0.1)
+        self.assertTrue(clean_float(1) == 1.0)
+        self.assertTrue(clean_float('c') == 0)
+        self.assertTrue(clean_float(None) == 0)
+
+    def test_clean_string(self):
+        self.assertTrue(clean_string("test") == "test")
+        self.assertTrue(clean_string("<test>") == "test")
+        self.assertTrue(clean_string(0) == "")
+        self.assertTrue(clean_string(10) == "")
+        self.assertTrue(clean_string("") == "")
+
+    def test_clean_boolean(self):
+        self.assertFalse(clean_boolean('False'))
+        self.assertFalse(clean_boolean('false'))
+        self.assertFalse(clean_boolean('0'))
+        self.assertTrue(clean_boolean('True'))
+        self.assertTrue(clean_boolean('true'))
+        self.assertTrue(clean_boolean('1'))
+        self.assertTrue(clean_boolean('') == "")
+        self.assertTrue(clean_boolean('test') == "")
+
+    def test_clean_yes_no(self):
+        self.assertTrue(clean_yes_no('no') == 'No')
+        self.assertTrue(clean_yes_no('yes') == 'Yes')
+        self.assertTrue(clean_yes_no('') == '')
+        self.assertTrue(clean_yes_no('anything else') == '')

kmullan/cfgov-refresh

cfgov/paying_for_college/disclosures/scripts/notifications.py
@@ -1,0 +1,62 @@
+import datetime
+from string import Template
+
+from django.core.mail import send_mail
+from django.utils import timezone
+
+from paying_for_college.models import Notification
+
+
+INTRO = ('Notification failures \n'
+         'Notification delivery failed for the following offer IDs:\n\n')
+NOTE_TEMPLATE = Template(('Offer ID $oid:\n'
+                          '    timestamp: $time\n'
+                          '    app errors: $errors\n'
+                          'SEND LOG:\n$log\n'))
+
+
+def retry_notifications(days=1):
+    """attempt to resend recent notifications that failed"""
+
+    endmsg = ""
+    days_old = timezone.now() - datetime.timedelta(days=int(days))
+    failed_notifications = Notification.objects.filter(sent=False,
+                                                       timestamp__gt=days_old)
+    for each in failed_notifications:
+        endmsg += "{}\n".format(each.notify_school())
+    if not endmsg:
+        endmsg = "No failed notifications found"
+    return endmsg
+
+
+def send_stale_notifications(add_email=[]):
+    """Gather up notifications that have failed and are more than a day old."""
+
+    stale_date = timezone.now() - datetime.timedelta(days=1)
+    stale_notifications = Notification.objects.filter(sent=False,
+                                                      timestamp__lt=stale_date)
+    if not stale_notifications:
+        return "No stale notifications found"
+    contacts = {notification.institution.contact: [] for notification
+                in stale_notifications if notification.institution.contact}
+    for noti in stale_notifications:
+        payload = {
+            'oid': noti.oid,
+            'time': noti.timestamp.isoformat(),
+            'errors': noti.errors,
+            'log': noti.log
+        }
+        clist = contacts[noti.institution.contact]
+        clist.append(payload)
+    for contact in contacts:
+        msg = INTRO
+        for msgdict in contacts[contact]:
+            msg += NOTE_TEMPLATE.substitute(msgdict)
+        recipients = contact.contacts.split(',') + add_email
+        send_mail("CFPB notification failures",
+                  msg,
+                  "no-reply@cfpb.gov",
+                  recipients,
+                  fail_silently=False)
+        return ("Found {} stale notifications; emails sent to "
+                "{}".format(stale_notifications.count(), recipients))

cfgov/paying_for_college/disclosures/scripts/purge_objects.py
@@ -1,0 +1,25 @@
+from paying_for_college.models import Notification, Program
+
+
+error_msg = ("The only purge arguments that can be passed "
+             "are 'notifications', 'programs' and 'test-programs'")
+no_args_msg = ("You must supply an object type to purge: "
+               "notifications, programs or test-programs")
+object_map = {
+    'notifications': Notification.objects.all(),
+    'programs': Program.objects.all(),
+    'test-programs': Program.objects.filter(test=True),
+}
+
+
+def purge(objects):
+    """purge notification or program data"""
+    objects = objects.lower()
+    if not objects:
+        return no_args_msg
+    if objects not in object_map.keys():
+        return error_msg
+    queryset = object_map[objects]
+    msg = "Purging {} {}".format(queryset.count(), objects)
+    queryset.delete()
+    return msg

cfgov/paying_for_college/management/commands/send_stale_notifications.py
@@ -1,0 +1,32 @@
+from django.core.management.base import BaseCommand
+
+from paying_for_college.disclosures.scripts.notifications import (
+    send_stale_notifications
+)
+
+
+COMMAND_HELP = "Send_stale_notifications gathers up stale notifications -- "
+"those that are more than a day old and have failed to reach a school -- "
+"assembles details and a send log for each notification and emails that "
+"to the school and to D&D team members for troubleshooting."
+PARSER_HELP = "This command queries the database for notifications with "
+"a 'sent' value of False and a timestamp more than a day old. "
+"You can pass 'add-email' and a list of emails to send logs to in addition "
+"to the school contact."
+
+
+class Command(BaseCommand):
+    help = COMMAND_HELP
+
+    def add_arguments(self, parser):
+        parser.add_argument('--add-email',
+                            help=PARSER_HELP,
+                            nargs='+',
+                            type=str)
+
+    def handle(self, *args, **options):
+        if options['add_email']:
+            msg = send_stale_notifications(add_email=options['add_email'])
+        else:
+            msg = send_stale_notifications()
+        self.stdout.write(msg)

cfgov/paying_for_college/management/commands/tag_schools.py
@@ -1,0 +1,23 @@
+from django.core.management.base import BaseCommand
+
+from paying_for_college.disclosures.scripts import tag_settlement_schools
+
+
+COMMAND_HELP = """
+`tag_schools` updates the 'settlement_school' flag, which is used to mark
+ schools that are participating in the disclosure program pursuant
+ to a legal settlement. The command accepts one argument, an S3 URL, which
+ should point to a CSV of schools subject to settlement. The CSV must contain
+ columns for 'ipeds_unit_id' and 'flag'.
+"""
+
+
+class Command(BaseCommand):
+    help = COMMAND_HELP
+
+    def add_arguments(self, parser):
+        parser.add_argument('url', type=str)
+
+    def handle(self, *args, **options):
+        msg = tag_settlement_schools.tag_schools(options['url'])
+        self.stdout.write(msg)

cfgov/paying_for_college/management/commands/update_pfc_national_stats.py
@@ -1,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from paying_for_college.disclosures.scripts import nat_stats
+
+
+COMMAND_HELP = """update_pfc_national_stats gets the latest national statistics
+ yaml file from collegescorecard, parses it and updates our local json file at
+ college/fixtures/national_stats.json.
+"""
+
+
+class Command(BaseCommand):
+    help = COMMAND_HELP
+
+    def handle(self, *args, **options):
+        msg = nat_stats.update_national_stats_file()
+        self.stdout.write(msg)

cfgov/paying_for_college/tests/test_validators.py
@@ -1,0 +1,106 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from paying_for_college.validators import (
+    clean_boolean, clean_float, clean_integer, clean_string, clean_yes_no,
+    validate_uuid4
+)
+
+
+INVALID_UUID = """
+\r\n\r\nOMG CHECK OUT THIS TOTES LEGIT CFPB-APPROVED WEBSITE: /
+http://nyan.cat\r\n\r\nOh, and here is some Kafka:\r\n\r\nOne morning, /
+as Gregor Samsa was waking up from anxious dreams, /
+he discovered that in his bed he had been changed into a monstrous /
+verminous bug. He lay on his armour-hard back and saw, as he lifted /
+his head up a little, his brown, arched abdomen divided up into rigid /
+bow-like sections. From this height the blanket, just about ready /
+to slide off completely, could hardly stay in place. His numerous legs, /
+pitifully thin in comparison to the rest of his circumference, /
+flickered helplessly before his eyes.\r\n
+"""
+
+VALID_WORKSHEET_DATA = """{"1": \
+{"school":"University of Kansas",\
+"alias":"Jayhawks",\
+"netpriceok":500,\
+"control":"Public",\
+"oncampusavail":"Yes",\
+"tuitiongradoss":"",\
+"offerba":"Yes",\
+"state":"KS",\
+"books":500,\
+"online":"No",\
+"school_id":"155317"}}
+"""
+
+INVALID_WORKSHEET_DATA = """{"1": {"school": \
+"University of Kansas | Jayhawks'}}
+"""
+
+DIRTY_WORKSHEET_DATA = """{"1": {"school": \
+"<script>console.log('INVALID');</script>"}}
+"""
+
+
+class UUIDValidatorTestCase(TestCase):
+    def test_valid_uuid4(self):
+        result = validate_uuid4('841df17e-784f-4ea4-bfb3-ebac5d2fcfe5')
+        # UUID validator does not return a value, only indications are raising
+        # ValidationError, or not
+        self.assertEqual(result, None)
+
+    def test_invalid_uuid4(self):
+        self.assertRaises(ValidationError, validate_uuid4, INVALID_UUID)
+
+
+class WorkSheetValidatorTestCase(TestCase):
+
+    # def test_valid_worksheet_data(self):
+    #     result = validate_worksheet(VALID_WORKSHEET_DATA)
+    #     self.assertTrue('Jayhawks' in result)
+
+    # def test_invalid_worksheet_data(self):
+    #     self.assertRaises(ValidationError,
+    #                       validate_worksheet,
+    #                       INVALID_WORKSHEET_DATA)
+
+    # def test_dirty_worksheet_data(self):
+    #     result = validate_worksheet(DIRTY_WORKSHEET_DATA)
+    #     self.assertFalse('<' in result)
+
+    def test_clean_integer(self):
+        self.assertTrue(clean_integer(1) == 1)
+        self.assertTrue(clean_integer(1.1) == 1)
+        self.assertTrue(clean_integer(0) == 0)
+        self.assertTrue(clean_integer('c') == 0)
+        self.assertTrue(clean_integer(None) == 0)
+
+    def test_clean_float(self):
+        self.assertTrue(clean_float(0.1) == 0.1)
+        self.assertTrue(clean_float(1) == 1.0)
+        self.assertTrue(clean_float('c') == 0)
+        self.assertTrue(clean_float(None) == 0)
+
+    def test_clean_string(self):
+        self.assertTrue(clean_string("test") == "test")
+        self.assertTrue(clean_string("<test>") == "test")
+        self.assertTrue(clean_string(0) == "")
+        self.assertTrue(clean_string(10) == "")
+        self.assertTrue(clean_string("") == "")
+
+    def test_clean_boolean(self):
+        self.assertFalse(clean_boolean('False'))
+        self.assertFalse(clean_boolean('false'))
+        self.assertFalse(clean_boolean('0'))
+        self.assertTrue(clean_boolean('True'))
+        self.assertTrue(clean_boolean('true'))
+        self.assertTrue(clean_boolean('1'))
+        self.assertTrue(clean_boolean('') == "")
+        self.assertTrue(clean_boolean('test') == "")
+
+    def test_clean_yes_no(self):
+        self.assertTrue(clean_yes_no('no') == 'No')
+        self.assertTrue(clean_yes_no('yes') == 'Yes')
+        self.assertTrue(clean_yes_no('') == '')
+        self.assertTrue(clean_yes_no('anything else') == '')